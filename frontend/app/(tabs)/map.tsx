d93dcbb80a46bb327c365f4438361b52a5c5dd6f
yThanos <vitorfraporti@hotmail.com>
2024-08-31 01:02:49 -0300

    aa

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
-import MapView, { LatLng, MapMarker, MapPolygon, Marker } from "react-native-maps";
+import { useEffect, useState } from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import MapView, { LatLng, Marker, MarkerPressEvent, Polygon } from "react-native-maps";
 
 interface SafePlace {
     coordinate: LatLng,
@@ -8,11 +8,19 @@ interface SafePlace {
     description: string
 }
 
+interface RiskArea {
+    coordinates: LatLng[],
+}
+
 export default function Map() {
-    const [riskAreas, setRiskAreas] = useState<MapPolygon[]>([]);
+    const [riskAreas, setRiskAreas] = useState<RiskArea[]>([]);
     const [safePlaces, setSafePlaces] = useState<SafePlace[]>([]);
 
-    ()=>{
+    const onMarkerPress = (e: MarkerPressEvent) => {
+
+    }
+
+    useEffect(()=>{
         setSafePlaces([
             {
                 coordinate: {latitude: -29.691828842370427, longitude:-53.80606971771388},
@@ -20,7 +28,19 @@ export default function Map() {
                 description: "Descrição hospital"
             }
         ])
-    }
+
+        setRiskAreas([
+            {
+                coordinates: [
+                    {latitude: -29.678230565195655, longitude: -53.82657792752787},
+                    {latitude: -29.676098510850235, longitude: -53.80983339419876},
+                    {latitude: -29.683121577855378, longitude: -53.809111647072505},
+                    {latitude: -29.68275195554361, longitude: -53.81800649362528},
+                    {latitude: -29.678230565195655, longitude: -53.82657792752787}
+                ]
+            }
+        ])
+    }, [])
 
     return (
         <SafeAreaView style={styles.contaienr}>
@@ -31,6 +51,7 @@ export default function Map() {
                     latitudeDelta: 0.05,
                     longitudeDelta: 0.05
                 }}
+                onMarkerPress={onMarkerPress}
             >
                 {safePlaces.map((marker, index)=>(
                     <Marker
@@ -38,6 +59,13 @@ export default function Map() {
                         coordinate={marker.coordinate}
                         title={marker.title}
                         description={marker.description}
+                        image={require("@/assets/images/safeplace.png")}
+                    />
+                ))}
+                {riskAreas.map((area, index)=>(
+                    <Polygon key={index}
+                        coordinates={area.coordinates}
+                        fillColor="#FF000060"
                     />
                 ))}
             </MapView>
