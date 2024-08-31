0f30c269cbe119c0e38851d0b5ce275c3a00f946
KEVIN <contatokevinlopes@gmail.com>
2024-08-31 04:33:02 -0300

    ajuste

--- backend/src/notificacoes/notificacoes.module.ts
+++ backend/src/notificacoes/notificacoes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificacoesService } from './notificacoes.service';
 import { NotificacoesController } from './notificacoes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notificacoes } from './entities/notificacoes.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Notificacoes])],
   controllers: [NotificacoesController],
   providers: [NotificacoesService],
 })
